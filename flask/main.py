34e5d2fb371b0cd58c1103767e3c5dbab8c22076
mystic <215104920@qq.com>
2020-09-18 10:27:12 +0800

    Learning Flask

---
+++ flask/main.py
@@ -0,0 +1,40 @@
+from flask import Flask, url_for
+from werkzeug.utils import redirect
+
+from middleware.operation_log import OperationLogMiddleware
+
+app = Flask(__name__)
+# add the custom middleware
+app.wsgi_app = OperationLogMiddleware(app.wsgi_app)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello World'
+
+
+@app.route('/hello/<name>')
+def hello(name: str):
+    return f'Hello {name}!'
+
+
+@app.route('/apply/<name>')
+def hello_redirect(name: str):
+    if name == 'admin':
+        return redirect(url_for('hello_admin'))
+    else:
+        return redirect(url_for('hello_guest', guest=name))
+
+
+@app.route('/admin')
+def hello_admin():
+    return 'Hello Admin'
+
+
+@app.route('/guest/<guest>')
+def hello_guest(guest):
+    return f'Hello {guest} as Guest'
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ flask/middleware/operation_log.py
@@ -0,0 +1,31 @@
+import logging
+
+from werkzeug import Request, Response
+
+AUDIT_LOG = 'cc.log'
+LOG = logging.getLogger(__name__)
+LOG.addHandler(logging.FileHandler(AUDIT_LOG))
+
+
+class OperationLogMiddleware:
+    def __init__(self, app):
+        self._app = app
+
+    def __call__(self, environ, start_response):
+        req = Request(environ)
+        resp = Response(start_response)
+        self._process_request(req)
+        self._process_response(resp)
+        return self._app(environ, start_response)
+
+    @staticmethod
+    def _process_request(request):
+        with open(AUDIT_LOG, 'a+', encoding='UTF-8') as f:
+            print(f'hello request: {request.method}', file=f)
+        LOG.info(f'hello world: {request}')
+
+    @staticmethod
+    def _process_response(response):
+        with open(AUDIT_LOG, 'a+', encoding='UTF-8') as f:
+            print(f'hello response: {response.status_code}', file=f)
+        LOG.info(f'hello world: {response}')
