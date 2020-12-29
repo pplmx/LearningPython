8459455b75d3a6900f04edf9c0e16a1c496c7d4f
mystic <215104920@qq.com>
2020-12-29 12:10:05 +0800

    [LeetCode] rm duplicates

---
+++ leetcode/00026_rm_duplicate.py
@@ -0,0 +1,34 @@
+from typing import List
+
+
+class Solution:
+    def remove_duplicates(self, nums: List[int]) -> int:
+        """
+
+        :param nums: It is a sorted array
+        :type nums:
+        :return:
+        :rtype:
+        """
+        nums[:] = sorted(set(nums))
+        return len(nums)
+
+    def normal_rm_duplica(self, nums: List[int]) -> int:
+        ret_len = 0
+        if len(nums) == ret_len:
+            return ret_len
+        ret_len = 1
+        for i in range(len(nums) - 1):
+            if nums[i] != nums[i + 1]:
+                nums[ret_len] = nums[i + 1]
+                ret_len += 1
+        nums[:] = nums[:ret_len]
+        return ret_len
+
+
+if __name__ == '__main__':
+    s = Solution()
+    arr = [1, 1, 1, 1, 2]
+    length = s.normal_rm_duplica(arr)
+    print(length)
+    print(arr)
