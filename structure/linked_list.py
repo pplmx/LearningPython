a1d5a8dab63fb577398de3cb1677076f700ed7d5
Mystic <215104920@qq.com>
2021-06-10 14:15:12 +0800

    feat(structure): linked list

---
+++ structure/linked_list.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Node class
+class Node:
+
+    # Function to initialize the node object
+    def __init__(self, data):
+        self.data = data  # Assign data
+        self.next = None  # Initialize next as null
+
+
+# Linked List class contains a Node object
+class LinkedList:
+
+    # Function to initialize head
+    def __init__(self):
+        self.head = None
+
+    def __repr__(self):
+        temp = self.head
+        p_str = ''
+        while temp:
+            p_str += f'{temp.data} -> '
+            temp = temp.next
+        return p_str[:-4]
+
+
+class SinglyLinkedList(LinkedList):
+    ...
+
+
+class DoublyLinkedList(LinkedList):
+    ...
+
+
+class CircularLinkedList(LinkedList):
+    ...
+
+
+def test_singly_linked_list():
+    n1 = Node(1)
+    n2 = Node(3)
+    n3 = Node(5)
+    n4 = Node(7)
+    n5 = Node(9)
+    s_l_list = SinglyLinkedList()
+    s_l_list.head = Node(0)
+    s_l_list.head.next = n1
+    n1.next = n2
+    n2.next = n3
+    n3.next = n4
+    n4.next = n5
+
+    print(s_l_list)
+
+
+if __name__ == '__main__':
+    test_singly_linked_list()
