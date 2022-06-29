55adbbf3799f3b7fd1d46a972746277669c7d132
Mystic <215104920@qq.com>
2022-06-29 16:50:09 +0800

    feat(bp): bp linear

--- ml/bp_linear.py
+++ ml/bp_linear.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+from utils.common import time_cost_ns
 
 
 def target_func(w, b):
@@ -8,23 +9,30 @@ def target_func(w, b):
     return x, y, z
 
 
+@time_cost_ns
 def double_variable_right(w, b, target_z):
     error = 1e-5
     idx = 0
     while True:
         x, y, z = target_func(w, b)
         delta_z = z - target_z
-        print(f'weight={w}, bias={b}, delta_z={delta_z}')
+        print(f'weight={w}, bias={b}, Δz={delta_z}')
         if abs(delta_z) < error:
             break
         idx += 1
         delta_w = 0.5 * delta_z / (2 * y)
         delta_b = 0.5 * delta_z / (3 * y + 2 * x)
-        print(f'delta_w={delta_w}, delta_b={delta_b}')
+        print(f'Δw={delta_w}, Δb={delta_b}')
         w -= delta_w
         b -= delta_b
 
-    print(f'The final weight = {w}, bias = {b}, Iterate times: {idx}')
+    print(f'''
+OUTPUT:
+    weight = {w}
+    bias = {b}
+    Δz = {delta_z}
+    Iterate times: {idx}
+    ''')
 
 
 if __name__ == '__main__':
---
+++ utils/common.py
@@ -0,0 +1,25 @@
+# time_cost with nanosecond
+import time
+
+
+def time_cost(func):
+    def wrapper(*args, **kwargs):
+        start = time.time()
+        result = func(*args, **kwargs)
+        end = time.time()
+        print(f'Cost: {end - start}s')
+        return result
+
+    return wrapper
+
+
+# count the cost of func in nanosecond
+def time_cost_ns(func):
+    def wrapper(*args, **kwargs):
+        start = time.time_ns()
+        result = func(*args, **kwargs)
+        end = time.time_ns()
+        print(f'Cost: {end - start}ns')
+        return result
+
+    return wrapper
