0a961c9965041bb27a58bea2ebb54ac56a8da6b3
Mystic <215104920@qq.com>
2024-07-21 18:22:05 +0800

    chore: some minor changes

--- base/singleton.py
+++ base/singleton.py
@@ -16,6 +16,24 @@ class Single:
         return obj
 
 
+class SingletonMeta(type):
+    _instances = {}  # 存储类的唯一实例
+
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            instance = super().__call__(*args, **kwargs)
+            cls._instances[cls] = instance
+        return cls._instances[cls]
+
+
+class SingletonClass(metaclass=SingletonMeta):
+    def __init__(self, value):
+        self.value = value
+
+    def display_value(self):
+        print(f'The value is: {self.value}')
+
+
 if __name__ == "__main__":
     s1 = Singleton()
     s2 = Singleton()
@@ -27,3 +45,14 @@ if __name__ == "__main__":
 
     print(s3.shared_attr)
     print(s3.shared_func(3))
+    print(s1 is s2)  # False, 这是 Borg's Singleton, 每个实例都是新的, 只是状态是共享的
+    print(s1.__class__)  # True
+
+    instance1 = SingletonClass(10)
+    instance2 = SingletonClass(20)
+
+    instance1.display_value()  # 输出: The value is: 10
+    instance2.display_value()  # 也输出: The value is: 10
+
+    print(instance1 is instance2)  # 输出: True
+    print(instance1.__class__)
